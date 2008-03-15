4fe72405b1898c24e9eeaf656de38a97b45a091e
Joshua Clayton <jclayton@fusionary.com>
2008-03-15 17:51:15 -0400

    Changed cap files

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,15 +11,6 @@ set :server, "coloma.joyent.us"
 set :mongrel_port, 4011
 set :mongrel_servers, 1
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-
-
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-
 role :app, server
 role :web, server
 role :db,  server, :primary => true

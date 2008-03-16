294e0241ea5e9716a0aba719d5190da7b7e3a9fa
Joshua Clayton <jclayton@fusionary.com>
2008-03-15 22:35:52 -0400

    Modified deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,17 @@
 set :application, "jdclayton"
 
 set :user, 'jclayton'
-set :deploy_to, "/users/home/#{user}/web"
+set :deploy_to, "/users/home/#{user}/apps/#{application}"
 
 set :scm, :git
+
 set :repository,  "git@github.com:joshuaclayton/jdclayton.git"
-set :deploy_vid, :remote_cache
+# set :deploy_vid, :remote_cache
 
 set :server, "coloma.joyent.us"
 set :mongrel_port, 4011
 set :mongrel_servers, 1
 
-role :app, server
-role :web, server
-role :db,  server, :primary => true
+role :app, "#{server}"
+role :web, "#{server}"
+role :db,  "#{server}", :primary => true

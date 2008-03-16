6c8162be0ddd6a487adae24b2c783fb1361787a0
Joshua Clayton <jclayton@fusionary.com>
2008-03-15 22:59:29 -0400

    Modified deploy to symlink directories correctly

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,3 +15,18 @@ set :mongrel_servers, 1
 role :app, "coloma.joyent.us"
 role :web, "coloma.joyent.us"
 role :db,  "coloma.joyent.us", :primary => true
+
+task :update_config, :roles => [:app] do
+  run "cp -Rf #{shared_path}/config/* #{release_path}/config/"
+end
+
+task :symlink_logs, :roles => [:app] do 
+  run "ln -s #{release_path}/log #{shared_path}/log"
+end
+
+task :symlink_web, :roles => [:app] do 
+  run "ln -s /users/home/#{user}/web #{release_path}"
+end
+
+after "deploy:update_code", [:update_config, :symlink_logs, :symlink_web]
+  

ec7e4827363ade33bc4a845992d662a86579193d
Joshua Clayton <jclayton@fusionary.com>
2008-03-16 13:40:07 -0400

    More tweaks to mongrel rb

--- config/mongrel.rb
+++ config/mongrel.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 # mongrel-based overrides of the default tasks
 
 namespace :deploy do
@@ -5,9 +6,10 @@ namespace :deploy do
     [ :stop, :start, :restart ].each do |t|
       desc "#{t.to_s.capitalize} the mongrel appserver"
       task t, :roles => :app do
+        root = `pwd`.to_s.strip
+        settings = YAML::load(File.new(File.join(root, "config", "mongrel_cluster.yml")).read)
         #invoke_command checks the use_sudo variable to determine how to run the mongrel_rails command
-        # invoke_command "mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method
-        invoke_command "mongrel_rails #{t.to_s} -C #{mongrel_conf}", :via => run_method
+        invoke_command "mongrel_rails #{t.to_s} --pid #{settings["pid_file"]}#{" -C #{mongrel_conf}" if t == :start}", :via => run_method
       end
     end
   end
